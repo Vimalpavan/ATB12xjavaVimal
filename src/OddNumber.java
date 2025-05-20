13485062e18116f337a5226ec08187d6329c4c9e
Vimalpavan <vimalpavan279@gmail.com>
2025-05-20 22:25:38 +0530

    code for odd number

---
+++ src/OddNumber.java
@@ -0,0 +1,10 @@
+public class OddNumber{
+    public static void main(String[] args) {
+        int number = 200;
+        for (int i =1 ; i<=number; i++) {
+            if (i % 2 !=0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
